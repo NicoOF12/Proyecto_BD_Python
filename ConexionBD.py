4bd10280bca0ee2e3c1f2197f3a3101192bae4db
NicoOF12 <114257471+NicoOF12@users.noreply.github.com>
2025-03-20 21:45:40 -0500

    Update ConexionBD.py

--- ConexionBD.py
+++ ConexionBD.py
@@ -1,19 +1,30 @@
 # main.py
+
 import mysql.connector
 from mysql.connector import Error
 
+# Configuración de la base de datos MySQL
 mysql_config = {
-    'host': '192.168.80.21',
-    'port': 3300,
-    'user': 'root',
-    'password': '1234',
-    'database': 'db_hospitales'
+    'host': '',  # Dirección IP del servidor MySQL
+    'port': 3300,             # Puerto de conexión a MySQL
+    'user': 'root',           # Usuario de la base de datos
+    'password': '',       # Contraseña del usuario
+    'database': 'db_hospitales'  # Nombre de la base de datos a usar
 }
 
 def get_connection():
+    """
+    Establece una conexión con la base de datos MySQL usando la configuración definida en mysql_config.
+
+    Returns:
+        mysql.connector.connection.MySQLConnection | None: 
+        Retorna un objeto de conexión a la base de datos si la conexión es exitosa,
+        de lo contrario, retorna None e imprime el error en la consola.
+    """
     try:
         connection = mysql.connector.connect(**mysql_config)
         return connection
     except Error as e:
-        print(f"Error during connection: {e}")
+        print(f"Error durante la conexión: {e}")
         return None
+
